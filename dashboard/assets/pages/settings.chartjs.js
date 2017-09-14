04a1f16b3b6d9b57cf17a0fbb1a52611137c1334
repsky <repsky@iMac-de-repsky.local>
2017-09-14 01:23:26 -0300

    dahsboard v0.5

--- dashboard/assets/pages/settings.chartjs.js
+++ dashboard/assets/pages/settings.chartjs.js
@@ -1,30 +1,30 @@
 new Chart(document.getElementById("mixed-chart"), {
     type: 'bar',
     data: {
-      labels: ["00", "1", "2", "3", "4", "5", "6", "7", "8", "9","10","11","11","12","13","14","15","16","17","18","19","20","21","22","23"],
+      labels: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9","10","11","11","12","13","14","15","16","17","18","19","20","21","22","23"],
       datasets: [{
           label: "Estoque",
           type: "line",
-          borderColor: "#FBB614",
-          data: [900,1221,783,1500],
+          borderColor: "#2EC4B6",
+          data: [900,1221,783,600,580,570,540,590,400,600],
           fill: false
         }, {
           label: "Processado",
           type: "bar",
           backgroundColor: "#E44921",
-          data: [0,700,547,478,734],
+          data: [0,700,0,478,734,0,700,0,478,734],
         }, {
           label: "Impresso",
           type: "bar",
-          backgroundColor: "#3e95cd",
-          data: [408,547,675,734],
+          backgroundColor: "#011627",
+          data: [200,408,547,675,734,200,408,547,675,734],
         }
       ]
     },
     options: {
       title: {
-        display: true,
-        text: 'Population growth (millions): Europe & Africa'
+        display: false,
+        text: 'estoque - posição 24hs'
       },
       legend: { display: true }
     }
